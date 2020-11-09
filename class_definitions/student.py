80b6e0fc6608e124822e00d462c6d6501fc35fca
Vicka93 <57420845+Vicka93@users.noreply.github.com>
2020-11-08 23:37:27 -0600

    All tests passed

--- class_definitions/student.py
+++ class_definitions/student.py
@@ -4,12 +4,25 @@ Program:student.py
 Student class
 """
 class Student:
+    MAJORS = ("English","CompSci")
     """Student class"""
     def __init__(self, lname, fname, major, gpa=0.0):
+        name_characters = set("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'-")
+        gpa_characters = set("1234567890-")
+        if not (name_characters.issuperset(lname) and name_characters.issuperset(fname)):
+            raise ValueError
+        if major not in self.MAJORS:
+            raise ValueError
+        if not isinstance(gpa,float):
+            raise ValueError
+        if not 0.0 <= gpa <= 4.0:
+            raise ValueError
+
         self.last_name = lname
         self.first_name = fname
         self.major = major
         self.gpa = gpa
 
     def __str__(self):
-        return self.last_name + ", " + self.first_name + " has major " + self.major + "with gpa: " + str(self.gpa)
+        return self.last_name + ", " + self.first_name + " has major " + self.major + " with gpa: " + str(self.gpa)
+
