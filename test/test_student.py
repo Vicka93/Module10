88c7803f4cc3bd0b5b907ec61783ab6c9f17a33a
Vicka93 <57420845+Vicka93@users.noreply.github.com>
2020-11-08 22:56:56 -0600

    passed test_object_created_required_attributes and test_object_created_all_attributes

--- test/test_student.py
+++ test/test_student.py
@@ -4,7 +4,7 @@ from class_definitions import student as t
 
 class MyTestCase(unittest.TestCase):
     def setUp(self):
-        self.student = t.Student('Denys', 'Daisy', 'English',4.0)
+        self.student = t.Student('Denys', 'Daisy', 'English')
 
     def tearDown(self):
         del self.student
@@ -13,10 +13,13 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(self.student.last_name, 'Denys')
         self.assertEqual(self.student.first_name, 'Daisy')
         self.assertEqual(self.student.major, 'English')
-        
 
     def test_object_created_all_attributes(self):
-        self.assertEqual(True, False)
+        student = t.Student('Denys', 'Daisy', 'English',4.0)
+        assert student.last_name == 'Denys'
+        assert student.first_name == 'Daisy'
+        assert student.major == 'English'
+        assert student.gpa == 4.0
 
     def test_student_str(self):
         self.assertEqual(True, False)
@@ -31,7 +34,8 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(True, False)
 
     def test_object_not_created_error_gpa(self):
-            self.assertEqual(True, False)
+        self.assertEqual(True, False)
+
 
 if __name__ == '__main__':
     unittest.main()
