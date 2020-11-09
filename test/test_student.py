8699eebec7c1d6d3a16909d225b6eb64c0c0564b
Vicka93 <57420845+Vicka93@users.noreply.github.com>
2020-11-08 23:37:15 -0600

    All tests passed

--- test/test_student.py
+++ test/test_student.py
@@ -15,27 +15,41 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(self.student.major, 'English')
 
     def test_object_created_all_attributes(self):
-        student = t.Student('Denys', 'Daisy', 'English',4.0)
+        student = t.Student('Denys', 'Daisy', 'English', 4.0)
         assert student.last_name == 'Denys'
         assert student.first_name == 'Daisy'
         assert student.major == 'English'
         assert student.gpa == 4.0
 
     def test_student_str(self):
+
         student = t.Student('Denys', 'Daisy', 'English',4.0)
+
         self.assertEqual(str(student),"Denys, Daisy has major English with gpa: 4.0")
 
     def test_object_not_created_error_last_name(self):
-        self.assertEqual(True, False)
+        with self.assertRaises(ValueError):
+            student = t.Student('123denys', 'Daisy', 'English', 4.0)
 
     def test_object_not_created_error_first_name(self):
-        self.assertEqual(True, False)
+        with self.assertRaises(ValueError):
+            student = t.Student('Denys', '123daisy','English', 4.0)
 
     def test_object_not_created_error_major(self):
-        self.assertEqual(True, False)
+        with self.assertRaises(ValueError):
+            student = t.Student('Denys', 'Daisy','123',4.0)
 
-    def test_object_not_created_error_gpa(self):
-        self.assertEqual(True, False)
+    def test_object_not_created_error_gpa_high(self):
+        with self.assertRaises(ValueError):
+            student = t.Student('Denys', 'Daisy', 'English',4.5)
+
+    def test_object_not_created_error_gpa_low(self):
+        with self.assertRaises(ValueError):
+            student = t.Student('Denys', 'Daisy', 'English',-4.5)
+
+    def test_object_not_created_error_gpa_bad(self):
+        with self.assertRaises(ValueError):
+            student = t.Student('Denys', 'Daisy', 'English',"four")
 
 
 if __name__ == '__main__':
